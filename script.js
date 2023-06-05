7092b8d00867b941fb97c59d8d632014cdaf4525
rubydevi <ariesabyss@gmail.com>
2023-06-05 18:43:09 +0530

    Add function to display books

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 const books = JSON.parse(localStorage.getItem('books')) || [];
 
+// Remove Book
+const removeBook = (index) => {
+  books.splice(index, 1);
+  localStorage.setItem('books', JSON.stringify(books));
+  const bookList = document.getElementById('bookList');
+  bookList.innerHTML = '';
+
+  books.forEach((book, index) => {
+    const listItem = document.createElement('li');
+    listItem.className = 'book-item';
+    listItem.innerHTML = `
+      <span>${book.title}</span>
+      <span>by ${book.author}</span>
+      <button class="remove-btn">Remove</button>
+      <hr>
+    `;
+    const removeButton = listItem.querySelector('.remove-btn');
+    removeButton.addEventListener('click', () => {
+      removeBook(index);
+    });
+    bookList.appendChild(listItem);
+  });
+};
+
+// Display all the books
+const displayBooks = () => {
+  const bookList = document.getElementById('bookList');
+  bookList.innerHTML = '';
+
+  books.forEach((book, index) => {
+    const listItem = document.createElement('li');
+    listItem.className = 'book-item';
+    listItem.innerHTML = `
+      <span>${book.title}</span>
+      <span>by ${book.author}</span>
+      <button class="remove-btn">Remove</button>
+      <hr>
+    `;
+    const removeButton = listItem.querySelector('.remove-btn');
+    removeButton.addEventListener('click', () => {
+      removeBook(index);
+    });
+    bookList.appendChild(listItem);
+  });
+};
+
 // Add Book
 const addBook = (title, author) => {
   const book = { title, author };
@@ -9,18 +55,7 @@ const addBook = (title, author) => {
   displayBooks();
 };
 
-// Remove Book
-const removeBook = (index) => {
-  books.splice(index, 1);
-  localStorage.setItem('books', JSON.stringify(books));
-  // display book
-  displayBooks();
-};
-
-// Function to display all books saved in the collection
-function displayBooks() {
-  
-}
-
-// Initial display of books
+// Initial book list display
 displayBooks();
+
+document.getElementById('bookContent').addEventListener('submit', submitBookForm);
