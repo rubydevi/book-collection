9f368c9c04e880163f1518d1b0ce72bd75e0d715
rubydevi <ariesabyss@gmail.com>
2023-06-05 15:55:31 +0530

    Add addBook function to add Books

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let books = JSON.parse(localStorage.getItem('books')) || [];
+const books = JSON.parse(localStorage.getItem('books')) || [];
+
+// Add Book
+const addBook = (title, author) => {
+  const book = { title, author };
+  books.push(book);
+  localStorage.setItem('books', JSON.stringify(books));
+  // display book
+};
