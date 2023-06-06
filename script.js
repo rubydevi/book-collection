984ae8d6c6e3067c207b42c5b0afdf07f67b9334
rubydevi <ariesabyss@gmail.com>
2023-06-06 20:40:52 +0530

    Fix linter errors, removed a class called Book

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-}
-
 class AwesomeBooks {
   constructor() {
     this.books = JSON.parse(localStorage.getItem('books')) || [];
@@ -15,7 +8,7 @@ class AwesomeBooks {
   }
 
   addBook(title, author) {
-    const newBook = new Book(title, author);
+    const newBook = { title, author };
     this.books.push(newBook);
     localStorage.setItem('books', JSON.stringify(this.books));
     this.displayBooks();
@@ -34,9 +27,27 @@ class AwesomeBooks {
     this.addBook(titleInput.value, authorInput.value);
     titleInput.value = '';
     authorInput.value = '';
-  }  
+  }
 
+  displayBooks() {
+    this.bookList.innerHTML = '';
+
+    this.books.forEach((book, index) => {
+      const listItem = document.createElement('li');
+      listItem.className = 'table-row';
+      listItem.innerHTML = `
+      <span class="table-cell">"${book.title}" by ${book.author}</span>
+      <span class="table-cell"><button class="remove-btn">Remove</button></span>
+      `;
+      const removeButton = listItem.querySelector('.remove-btn');
+      removeButton.addEventListener('click', () => {
+        this.removeBook(index);
+      });
+      this.bookList.appendChild(listItem);
+    });
+  }
 }
 
 // Instantiate AwesomeBooks
 const bookCollection = new AwesomeBooks();
+bookCollection();
