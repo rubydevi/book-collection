029c00cef834bb8ea1c200a40fe15b9cf6467794
Dorcas126 <uwimbabazidorcas65@gmail.com>
2023-06-06 11:56:20 +0200

    Display books after removing book on the list

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ const books = JSON.parse(localStorage.getItem('books')) || [];
 const removeBook = (index) => {
   books.splice(index, 1);
   localStorage.setItem('books', JSON.stringify(books));
-  // please add here
+  const bookList = document.getElementById('bookList');
+  bookList.innerHTML = '';
+
+  books.forEach((book, index) => {
+    const listItem = document.createElement('li');
+    listItem.className = 'book-item';
+    listItem.innerHTML = `
+      <span>${book.title}</span>
+      <span>by ${book.author}</span>
+      <button class="remove-btn">Remove</button>
+      <hr>
+    `;
+    const removeButton = listItem.querySelector('.remove-btn');
+    removeButton.addEventListener('click', () => {
+      removeBook(index);
+    });
+    bookList.appendChild(listItem);
+  });
 };
 
 // Display all the books
